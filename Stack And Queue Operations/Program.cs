e259e3cc278fb4ebeec4307921ae32e307b0f97e
asmithasatesh <asmithasatesh@gmail.com>
2021-07-10 11:26:10 +0530

    [Asmitha]Initial commit with Welcome Message

---
+++ Stack And Queue Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack_And_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Operations!");
+        }
+    }
+}
---
+++ Stack And Queue Operations/StackAndQueue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_And_Queue_Operations
+{
+    class StackAndQueue
+    {
+
+    }
+}
