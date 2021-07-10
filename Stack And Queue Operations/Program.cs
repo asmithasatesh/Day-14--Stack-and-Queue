356c6bf5daf0f12efded0f161850dc43e188377b
asmithasatesh <asmithasatesh@gmail.com>
2021-07-10 11:46:38 +0530

    [Asmitha]Peek and Pop element from stack

--- Stack And Queue Operations/Program.cs
+++ Stack And Queue Operations/Program.cs
@@ -15,20 +15,36 @@ namespace Stack_And_Queue_Operations
             {
                 case 1:
                     Console.WriteLine("Enter 1-to Push Element to Stack");
-
+                    Console.WriteLine("Enter 2-to peek and Pop Element onStack");
                     int option = Convert.ToInt32(Console.ReadLine());
                     switch (option)
                     {
                         case 1:
-
-
                             //Push operations
                             stack.Push(70);
                             stack.Push(30);
                             stack.Push(56);
                             //Display operation
                             Console.Write("\n-----DISPLAYING STACK ELEMENTS-----\n\n");
-                            stack.Display();
+                            int total=stack.DisplayStack();
+                            break;
+                        case 2:
+
+                            //Push operations
+                            int count = 0;
+                            stack.Push(70);
+                            stack.Push(30);
+                            stack.Push(56);
+                            //Display operation
+                            Console.Write("\n-----DISPLAYING STACK ELEMENTS-----\n\n");
+                            count = stack.DisplayStack();
+
+                            //Pop operation
+                            for (int i = 0; i <= count; i++)
+                            {
+                                stack.Pop();
+
+                            }
                             break;
                     }
                     break;
